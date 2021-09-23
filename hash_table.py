06e24b302b8aa9b40d9578109bf1663b6ac1ed57
Jônatas Mendes da Cruz <62781857+Kyoto-01@users.noreply.github.com>
2021-09-23 17:20:51 -0300

    criado código inicial da tabela hash

---
+++ hash_table.py
@@ -0,0 +1,96 @@
+def is_prime(num: int) -> bool:
+    num = abs(num)
+    for i in range(2, (num // 2) + 1):
+        if num % i == 0:
+            return False
+    if num < 2:
+        return False
+    return True
+
+def max_prime(num: int) -> int:
+    for i in range(num, 1, -1):
+        if is_prime(i):
+            return i
+    return 2
+
+
+class Entry:
+    
+    def __init__(self, key: object, value: object):
+        self.key = key
+        self.value = value
+
+
+class HashTable:
+    
+    def __init__(self, length: int):
+        self.__length = length
+        self.__table: list['Entry'] = [None] * length
+        
+    def hashing(self, key: object) -> int:
+        return hash(key) % self.__length
+        
+    def hashing2(self, key: object) -> int:
+        prime = max_prime(self.length)
+        return prime - (hash(key) % prime)
+        
+    def re_hashing(self, key: object, i: int = 1):
+        return (self.hashing(key) + i * self.hashing2(key)) % self.__length
+    
+    def insert(self, key: object, value: object):
+        index = self.hashing(key)
+        
+        i = 1
+        while self.__table[index] is not None:
+            assert i < self.__length, 'Tabela cheia!'
+            index = self.re_hashing(key, i)
+            i += 1
+            
+        self.__table[index] = Entry(key, value)
+            
+        
+    def get(self, key: object) -> object:
+        index = self.hashing(key)
+        assert self.__table[index] is not None, 'Nenhuma entrada correspondente!'
+        
+        i = 1
+        while self.__table[index].key != key:
+            assert i < self.__length, 'Nenhuma entrada correspondente!'
+            index = re_hashing(key, i)
+            i += 1
+            
+        return self.__table[index].value
+        
+    def print_entries(self):
+        for k, v in enumerate(self.__table):
+            if v:
+                print(k, v.key, v.value)
+        
+        
+if __name__ == '__main__':
+    
+    # Teste
+    ports = HashTable(24)
+    
+    while True:
+        
+        print('''1 --> Inserir MAC/Port
+2 --> Pesquisar MAC/Port
+3 --> print ports
+''')
+        
+        opcao = input('opção --> ')
+        
+        if opcao == '1':
+            mac, port = input('\nDigite o MAC e a porta. Ex.: [ff:ff:ff:ff:ff:ff 24] -->').split()
+            ports.insert(mac, port)
+        elif opcao == '2':
+            mac = input('\nDigite um MAC --> ')
+            print('-->',mac)
+            port = ports.get(mac)
+            print(f'O MAC {mac} está na porta {port}')
+        elif opcao == '3':
+            ports.print_entries()
+        else:
+            break
+        
